1caf32a551b896f31615fe7168cd4212e426be22
joris <97834949+Georgyporgie@users.noreply.github.com>
2026-01-14 14:53:36 +0100

    Add files via upload

--- main3.js
+++ main3.js
@@ -25,7 +25,7 @@ let total_duration = document.querySelector(".total-duration");
 
 // Specify globally used values
 let track_index = 0;
-let isPlaying = false;
+let currentTrack = null;  
 let updateTimer;
  
 // Create the audio element for the player
